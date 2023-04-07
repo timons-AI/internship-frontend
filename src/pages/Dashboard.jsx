2525b679a053d30a8b247aa67985daa941b7fcc9
timons-AI <123802219+timons-AI@users.noreply.github.com>
2023-04-07 19:09:02 +0300

    Update Dashboard.jsx
    flex-col

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,7 +9,7 @@ const Dashboard = () => {
   
   return (
     <div>
-        <div className=' flex justify-between items-center p-2'>
+        <div className=' flex justify-between items-center p-2 flex-col'>
           <button className=' ease-out duration-300 hover:bg-gray-950 active:bg-white active:text-black bg-slate-600 p-2 font-bold text-white w-50 rounded-md' onClick={() => loginWithRedirect()}>Login</button>
           <button className=' ease-out duration-300 hover:bg-gray-950 active:bg-white active:text-black bg-slate-600 p-2 font-bold text-white w-50 rounded-md' onClick={() => logout()}>Logout</button>
           {isAuthenticated &&(
