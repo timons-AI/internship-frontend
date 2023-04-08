35dae2fa12970e3996606d90b3e06dd443a0e05c
mrbillionn <kyaligonzawarren44@gmail.con>
2023-04-09 00:01:42 +0300

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div style={{backgroundColor: '#1D1E22', color: 'white', padding: '50px'}}>
-      <h1>About Standard Intern</h1>
+    <div style={{color: 'white', padding: '10px'}}>
+      <br/><br/><h1>About Standard Intern</h1><br/>
       <p>At Standard Intern, we believe that every student and recent graduate deserves access to quality internship opportunities that align with their career goals. That's why we created a platform that connects students with top companies in their field, giving them the tools and resources they need to jumpstart their careers.</p>
-      <p>Our mission is simple: to make the internship search process as easy and stress-free as possible. Our website features a user-friendly interface that allows students to search for internships based on their skills, interests, and location. We partner with Fortune 500 companies and industry leaders to offer internships that provide students with valuable experience and help them build their professional networks.</p>
-      <p>With Standard Intern, students can browse internship opportunities, apply directly to companies, and receive personalized recommendations based on their preferences. We also provide resources and support to help students prepare for job interviews and succeed in their internships.</p>
-      <p>Join the thousands of students and recent graduates who have found success with Standard Intern. Start browsing internships today and take the first step towards a successful career.</p>
+      <br/><p>Our mission is simple: to make the internship search process as easy and stress-free as possible. Our website features a user-friendly interface that allows students to search for internships based on their skills, interests, and location. We partner with Fortune 500 companies and industry leaders to offer internships that provide students with valuable experience and help them build their professional networks.</p>
+      <br/><p>With Standard Intern, students can browse internship opportunities, apply directly to companies, and receive personalized recommendations based on their preferences. We also provide resources and support to help students prepare for job interviews and succeed in their internships.</p>
+      <br/><p>Join the thousands of students and recent graduates who have found success with Standard Intern. Start browsing internships today and take the first step towards a successful career.</p>
       <div style={{marginTop: '50px'}}>
-        <h2>Why Choose Standard Intern?</h2>
+        <br/><br/><h2>Why Choose Standard Intern?</h2>
         <ul>
-          <li>Access to quality internship opportunities</li>
+          <br/><li>Access to quality internship opportunities</li>
           <li>User-friendly interface for easy navigation</li>
           <li>Partnerships with top companies in various fields</li>
           <li>Personalized recommendations based on your preferences</li>
