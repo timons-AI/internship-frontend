3260358b88de82a6e34d148364ce62e081ea0b0b
mrbillionn <kyaligonzawarren44@gmail.con>
2023-04-08 22:43:49 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -134,7 +134,7 @@ const Home = () => {
 
 const Card = ({ company }) => {
   return (
-    <div className="flex flex-col p-4 bg-gray-200 rounded-md w-full  lg:w-1/3 m-2 h-1/5 text-xs">
+    <div className="flex flex-col p-4 bg-gray-200 rounded-lg w-full  lg:w-1/3 m-2 h-1/5 text-xs">
       <h2 ><span className='font-medium'>Name </span>: {company.company_name}</h2>
       {/* <p className="text-lg"><span className=' font-medium '>Description</span>: {company.description}</p> */}
       <p ><span className=' font-medium '>Region</span>: {company.region_name}</p>
