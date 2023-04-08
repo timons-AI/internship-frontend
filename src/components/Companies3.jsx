28d06c54c02847463c0080bcbfca60be9b413996
timons-AI <123802219+timons-AI@users.noreply.github.com>
2023-04-09 02:01:21 +0300

    Update Companies3.jsx
    d

--- src/components/Companies3.jsx
+++ src/components/Companies3.jsx
@@ -2,86 +2,91 @@ import React ,{ useState, useEffect } from 'react'
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Companies3 = () => {
-    const { getAccessTokenSilently } = useAuth0();
-    const [formData, setFormData] = useState({
-        name: '',
-        description: '',
-        address: '',
-        contact: '',
-        regions: [],
-        professions: [],
+  const { getAccessTokenSilently } = useAuth0();
+  const [formData, setFormData] = useState({
+    name: '',
+    description: '',
+    address: '',
+    contact: '',
+    regions: [],
+    professions: [],
+  });
+
+  const [regions, setRegions] = useState([]);
+  const [professions, setProfessions] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    fetch('https://intern-app-u7zql.ondigitalocean.app/api/regions')
+      .then((res) => res.json())
+      .then((data) => setRegions(data))
+      .catch((err) => console.error(err));
+
+    fetch('https://intern-app-u7zql.ondigitalocean.app/api/professions')
+      .then((res) => res.json())
+      .then((data) => setProfessions(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  const handleChange = ({ target: { name, value } }) => {
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleRegionChange = ({ target: { value } }) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      regions: prevData.regions.includes(value)
+        ? prevData.regions.filter((id) => id !== value)
+        : [...prevData.regions, value],
+    }));
+  };
+
+  const handleProfessionChange = ({ target: { value } }) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      professions: prevData.professions.includes(value)
+        ? prevData.professions.filter((id) => id !== value)
+        : [...prevData.professions, value],
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // validate inputs
+    if (!formData.name || !formData.description || !formData.address || !formData.contact) {
+      setError('Please fill in all the required fields.');
+      return;
+    }
+
+    try {
+      const res = await fetch('https://intern-app-u7zql.ondigitalocean.app/api/setAll', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${await getAccessTokenSilently()}`,
+        },
+        body: JSON.stringify(formData),
       });
-    
-      const [regions, setRegions] = useState([]);
-      const [professions, setProfessions] = useState([]);
-    
-      useEffect(() => {
-        fetch('https://intern-app-u7zql.ondigitalocean.app/api/regions')
-          .then((res) => res.json())
-          .then((data) => setRegions(data))
-          .catch((err) => console.error(err));
-    
-        fetch('https://intern-app-u7zql.ondigitalocean.app/api/professions')
-          .then((res) => res.json())
-          .then((data) => setProfessions(data))
-          .catch((err) => console.error(err));
-      }, []);
-    
-      const handleChange = (event) => {
-        const { name, value } = event.target;
-        setFormData((prevData) => ({ ...prevData, [name]: value }));
-      };
-    
-      const handleRegionChange = (event) => {
-        const { value } = event.target;
-        setFormData((prevData) => ({
-          ...prevData,
-          regions: prevData.regions.includes(value)
-            ? prevData.regions.filter((id) => id !== value)
-            : [...prevData.regions, value],
-        }));
-      };
-    
-      const handleProfessionChange = (event) => {
-        const { value } = event.target;
-        setFormData((prevData) => ({
-          ...prevData,
-          professions: prevData.professions.includes(value)
-            ? prevData.professions.filter((id) => id !== value)
-            : [...prevData.professions, value],
-        }));
-      };
-    
-      const handleSubmit = async (event) => {
-        
-        event.preventDefault();
-        try {
-          const res = await fetch('https://intern-app-u7zql.ondigitalocean.app/api/setAll', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: `Bearer ${await getAccessTokenSilently()}`,
-            },
-            body: JSON.stringify(formData),
-          });
-          if (res.ok) {
-            setFormData({
-              name: '',
-              description: '',
-              address: '',
-              contact: '',
-              regions: [],
-              professions: [],
-            });
-            alert('Company created successfully.');
-          } else {
-            throw new Error('Failed to create company.');
-          }
-        } catch (err) {
-          console.error(err);
-          alert('Failed to create company.');
-        }
-      };
+      if (res.ok) {
+        setFormData({
+          name: '',
+          description: '',
+          address: '',
+          contact: '',
+          regions: [],
+          professions: [],
+        });
+        setError('');
+        alert('Company created successfully.');
+      } else {
+        throw new Error('Failed to create company.');
+      }
+    } catch (err) {
+      console.error(err);
+      setError('Failed to create company.');
+    }
+  };
   return (
     <form onSubmit={handleSubmit}>
       <label>
