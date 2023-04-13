63d612d4c4a97c46e874a7e8fc63e30d138abdc3
timons-AI <123802219+timons-AI@users.noreply.github.com>
2023-04-13 17:36:16 +0300

    Update Home.jsx
    second largest

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -68,7 +68,7 @@ const Home = () => {
     <section className='flex flex-col justify-center items-center px-4 py-2'><br/><br/><br/>
     <h4 className='font-extralight text-center text-slate-100'>OVER 1000+ ACTIVE LISTINGS</h4>
       <h1 className='font-medium text-center text-slate-100 text-2xl'>Find nearby internship places</h1><br/>
-      <div className='flex flex-col justify-center items-center bg-gray-100 bg-opacity-25 p-3 w-full lg:w-90 rounded-xl'>
+      <div className='flex flex-col justify-center items-center bg-gray-100 bg-opacity-25 p-3 rounded-md w-full lg:w-1/2'>
         <div className='flex flex-col justify-center items-center my-2 w-full md:w-1/2'>
           <label className='text-xl font-medium text-slate-200 text-center'>Region</label>
           <select
