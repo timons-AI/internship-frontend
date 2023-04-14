ae5c5203ab1c796d07a19d3ba1067455064bdea3
mrbillionn <kyaligonzawarren44@gmail.con>
2023-04-15 02:26:09 +0300

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,10 +10,10 @@ const About = () => {
           At Standard Intern, we believe that every student and recent graduate deserves access to quality internship opportunities that align with their career goals. That's why we created a platform that connects students with top companies in their field, giving them the tools and resources they need to jumpstart their careers.
         </p>
         <p className="text-lg mb-8">
-          Our mission is simple: to make the internship search process as easy and stress-free as possible. Our website features a user-friendly interface that allows students to search for internships based on their skills, interests, and location. We partner with Fortune 500 companies and industry leaders to offer internships that provide students with valuable experience and help them build their professional networks.
+          Our mission is simple: to make the internship search process as easy and stress-free as possible. Our website features a user-friendly interface that allows students to search for internships based on their skills, interests, and location. We partner with various companies and industry leaders to offer internships that provide students with valuable experience and help them build their professional networks.
         </p>
         <p className="text-lg mb-8">
-          With Standard Intern, students can browse internship opportunities, apply directly to companies, and receive personalized recommendations based on their preferences. We also provide resources and support to help students prepare for job interviews and succeed in their internships.
+          With Standard Intern, students can browse internship opportunities, apply directly to companies in the future, and receive personalized recommendations based on their preferences. We also provide resources and support to help students prepare for job interviews and succeed in their internships.
         </p>
         <div className="mt-16">
           <h2 className="text-2xl font-bold mb-8">Why Use Standard Intern?</h2>
