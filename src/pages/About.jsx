d8e3d1cf477ba2fac0b60172196d0ab6c875265c
mrbillionn <kyaligonzawarren44@gmail.con>
2023-04-16 01:23:24 +0300

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,34 +4,33 @@ import FAQ from "../components/FAQ";
 const About = () => {
   return (
     <div className="bg-white text-black p-3 ">
-      <div className="container mx-auto">
-        <h1 className="text-4xl font-bold mb-8">About Standard Intern</h1>
-        <p className="text-lg mb-8">
-          At Standard Intern, we believe that every student and recent graduate deserves access to quality internship opportunities that align with their career goals. That's why we created a platform that connects students with top companies in their field, giving them the tools and resources they need to jumpstart their careers.
-        </p>
-        <p className="text-lg mb-8">
-          Our mission is simple: to make the internship search process as easy and stress-free as possible. Our website features a user-friendly interface that allows students to search for internships based on their skills, interests, and location. We partner with various companies and industry leaders to offer internships that provide students with valuable experience and help them build their professional networks.
-        </p>
-        <p className="text-lg mb-8">
-          With Standard Intern, students can browse internship opportunities, apply directly to companies in the future, and receive personalized recommendations based on their preferences. We also provide resources and support to help students prepare for job interviews and succeed in their internships.
-        </p>
-        <div className="mt-16">
-          <h2 className="text-2xl font-bold mb-8">Why Use Standard Intern?</h2>
-          <ul className="list-disc list-inside">
-            <li className="text-lg mb-4">Access to quality internship opportunities</li>
-            <li className="text-lg mb-4">User-friendly interface for easy navigation</li>
-            <li className="text-lg mb-4">Partnerships with top companies in various fields</li>
-            <li className="text-lg mb-4">Personalized recommendations based on your preferences</li>
-            <li className="text-lg mb-4">Resources and support to help you succeed in your internship</li>
-            <li className="text-lg mb-4">A community of like-minded students and professionals</li>
-          </ul>
-        </div>
-      </div>
-      
-      <section className="flex flex-col items-center justify-center">
-        <p className="text-2xl font-bold mb-8">Frequently Asked Questions</p>
-          <FAQ />
-      </section>
+      <div className="container">
+  <h1 className="text-center mb-5">About Standard Intern</h1>
+  <h2 className="mb-4">Our Mission and Values</h2>
+<p>
+At Standard Intern, Our mission is simple: to make the internship search process as easy and stress-free as possible. Our website features a user-friendly interface that allows students to search for internships based on their skills, interests, and location. We partner with various companies and industry leaders to offer internships that provide students with valuable experience and help them build their professional networks.
+</p>
+<h2 className="mb-4">Meet the Team</h2>
+<h3>Founder/CEO - John Smith</h3>
+<p>
+John Smith is the founder and CEO of Standard Intern. With over 10 years of experience in the tech industry, John has a deep understanding of the challenges faced by companies when it comes to finding top talent. He started Standard Intern with the goal of providing a more streamlined and efficient process for intern recruitment, while also creating opportunities for young professionals to gain valuable experience and build their careers.
+</p>
+<h3>CTO/Cofounder - Jane Doe</h3>
+<p>
+Jane Doe is the CTO and cofounder of Standard Intern. She is a highly skilled software engineer with a passion for building innovative solutions that solve real-world problems. Jane's expertise in technology has been instrumental in the development of Standard Intern's proprietary platform, which matches companies with interns based on their skills, interests, and career goals.
+</p>
+<h3>Contributors</h3>
+<ul>
+<li>Emily Johnson - Marketing Manager</li>
+<li>Michael Lee - Operations Manager</li>
+<li>Sara Kim - Customer Success Specialist</li>
+</ul>
+<h2 className="mb-4">Our Vision for the Future</h2>
+<p>
+Looking ahead, we see a future where intern recruitment is more efficient and effective than ever before. We are committed to continuing to improve our platform and services, and to expanding our reach to help companies and interns across the globe. We are also working on several exciting new projects, including a mentorship program and a training program for interns to develop specific skills.
+</p>
+<FAQ />
+</div>
      
     </div>
   );
