ded9328e872dfe9cfb808eb6be265ffb6be2db4a
mrbillionn <kyaligonzawarren44@gmail.con>
2023-04-16 01:50:57 +0300

    Update About.jsx
    about us page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,31 +1,32 @@
-import React from "react";
-import FAQ from "../components/FAQ";
+import React from "react"
 
 const About = () => {
   return (
     <div className="bg-white text-black p-3 ">
       <div className="container">
-  <h1 className="text-center mb-5">About Standard Intern</h1>
-  <h2 className="mb-4">Our Mission and Values</h2>
+  <h1 className="text-center font-bold">About Standard Intern</h1>
+  <br/><h2 className="mb-4">Our Mission and Values</h2>
 <p>
 At Standard Intern, Our mission is simple: to make the internship search process as easy and stress-free as possible. Our website features a user-friendly interface that allows students to search for internships based on their skills, interests, and location. We partner with various companies and industry leaders to offer internships that provide students with valuable experience and help them build their professional networks.
 </p>
-<h2 className="mb-4">Meet the Team</h2>
-<h3>Founder/CEO - John Smith</h3>
+<br/><h2 className="mb-4 font-bold">Meet the Team</h2>
+<h3 className="font-bold">Founder & CEO - Kyaligonza Warren Willis</h3>
 <p>
-John Smith is the founder and CEO of Standard Intern. With over 10 years of experience in the tech industry, John has a deep understanding of the challenges faced by companies when it comes to finding top talent. He started Standard Intern with the goal of providing a more streamlined and efficient process for intern recruitment, while also creating opportunities for young professionals to gain valuable experience and build their careers.
-</p>
-<h3>CTO/Cofounder - Jane Doe</h3>
+Kyaligonza Warren Willis is the visionary founder and CEO of Standard Intern. With a passion for solving the issue of unemployment, Kyaligonza started Standard Intern with the goal of providing a more streamlined and efficient process for intern recruitment, while also creating opportunities for young professionals to gain valuable experience and build their careers. Before starting Standard Intern, he attended Kings College Budo and is currently studying Information Systems and Technology. At the young age of 22, he has already made remarkable achievements in his career, including starting and owning Billion Fashion. His entrepreneurial spirit and dedication to providing opportunities for young professionals are at the heart of Standard Intern's mission. Warren's expertise in technology and his innovative ideas have been instrumental in the development of Standard Intern's proprietary platform, which matches companies with interns based on their skills, interests, and career goals. Looking ahead, Warren Willis has a bold vision for the future of the company and is committed to expanding its reach and impact on the intern recruitment industry.</p>
+<br/><h3 className="font-bold">CTO & Cofounder - Ntambi Timothy</h3>
 <p>
-Jane Doe is the CTO and cofounder of Standard Intern. She is a highly skilled software engineer with a passion for building innovative solutions that solve real-world problems. Jane's expertise in technology has been instrumental in the development of Standard Intern's proprietary platform, which matches companies with interns based on their skills, interests, and career goals.
-</p>
-<h3>Contributors</h3>
+Ntambi Timothy is the CTO and cofounder of Standard Intern. He is a highly skilled software engineer with expertise in AI, programming, and building innovative solutions that solve real-world problems. Timothy has completed several projects for individuals and organizations, showcasing his exceptional problem-solving skills and attention to detail. At just 22 years old, Timothy brings a fresh perspective to Standard Intern and has been instrumental in the development of the company's proprietary platform. His passion for using technology to improve people's lives is at the core of Standard Intern's mission, and his dedication to creating an intuitive and efficient user experience is reflected in the platform's design.</p>
+<br/><h3>Key Contributors</h3>
 <ul>
-<li>Emily Johnson - Marketing Manager</li>
-<li>Michael Lee - Operations Manager</li>
-<li>Sara Kim - Customer Success Specialist</li>
+<li>Cynthia Mary A - Sponsor</li>
+<li>Desire Della K - Sponsor</li>
+<li>Daniella Donna K  - Sponsor</li>
+<li>Susan Akello R - Sponsor</li>
+<li>Bwiiso Timothy - Sponsor</li>
+<li>Staha Nigel - Consultant</li>
+<li>Tevin Tumwine - Marketing</li>
 </ul>
-<h2 className="mb-4">Our Vision for the Future</h2>
+<br/><h2 className="mb-4 font-bold">Our Vision for the Future</h2>
 <p>
 Looking ahead, we see a future where intern recruitment is more efficient and effective than ever before. We are committed to continuing to improve our platform and services, and to expanding our reach to help companies and interns across the globe. We are also working on several exciting new projects, including a mentorship program and a training program for interns to develop specific skills.
 </p>
