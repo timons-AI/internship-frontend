39996884827dc5908faaadd38f3977a4b19a8987
mrbillionn <kyaligonzawarren44@gmail.con>
2023-04-18 19:53:10 +0300

    Update Contact.jsx
    contact us page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,71 @@
+import React from 'react';
+
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
+    <div className="bg-white text-black p-3">
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
+      <div className="flex flex-wrap mb-4">
+        <div className="w-full sm:w-1/2 md:w-1/4 px-4 mb-4">
+          <a href="mailto:standardintern@gmail.com">
+            <div className="bg-gray-100 p-4 text-center rounded-lg">
+              <i className="fas fa-envelope text-4xl mb-4"></i>
+              <p className="font-bold mb-2">Email Us</p>
+              <p className="text-gray-700">standardintern@gmail.com</p>
+            </div>
+          </a>
+        </div>
+        <div className="w-full sm:w-1/2 md:w-1/4 px-4 mb-4">
+          <a href="https://twitter.com/standardintern">
+            <div className="bg-gray-100 p-4 text-center rounded-lg">
+              <i className="fab fa-twitter text-4xl mb-4"></i>
+              <p className="font-bold mb-2">Follow us on Twitter</p>
+              <p className="text-gray-700">@standardintern</p>
+            </div>
+          </a>
+        </div>
+        <div className="w-full sm:w-1/2 md:w-1/4 px-4 mb-4">
+          <a href="https://www.instagram.com/standardintern/">
+            <div className="bg-gray-100 p-4 text-center rounded-lg">
+              <i className="fab fa-instagram text-4xl mb-4"></i>
+              <p className="font-bold mb-2">Follow us on Instagram</p>
+              <p className="text-gray-700">@standardintern</p>
+            </div>
+          </a>
+        </div>
+        <div className="w-full sm:w-1/2 md:w-1/4 px-4 mb-4">
+          <a href="https://wa.me/+256708163155?text=Hello there">
+            <div className="bg-gray-100 p-4 text-center rounded-lg">
+              <i className="fab fa-whatsapp text-4xl mb-4"></i>
+              <p className="font-bold mb-2">Message us on WhatsApp</p>
+              <p className="text-gray-700">+256-708-163-155</p>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div className="flex flex-wrap">
+        <div className="w-full sm:w-1/2 md:w-1/4 px-4 mb-4">
+          <a href="tel:+256708163155">
+            <div className="bg-gray-100 p-4 text-center rounded-lg">
+              <i className="fas fa-phone-alt text-4xl mb-4"></i>
+              <p className="font-bold mb-2">Call us</p>
+              <p className="text-gray-700">+256-708-163-155</p>
+            </div>
+          </a>
+        </div>
+</div>
+<div className="w-full sm:w-1/2 md:w-1/4 px-4 mb-4">
+<a href="mailto:standardintern@gmail.com">
+<div className="bg-gray-100 p-4 text-center rounded-lg">
+<i className="fas fa-comment-alt text-4xl mb-4"></i>
+<p className="font-bold mb-2">Send us a message</p>
+<p className="text-gray-700">Fill out the form to send us a message.</p>
+</div>
+</a>
+</div>
+</div>
+</div>
+)
 }
 
 export default Contact
