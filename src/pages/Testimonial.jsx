01fc48abf71f6669b9201afee599b4aabbbdc782
mrbillionn <kyaligonzawarren44@gmail.con>
2023-04-18 19:20:37 +0300

    Update Testimonial.jsx

--- src/pages/Testimonial.jsx
+++ src/pages/Testimonial.jsx
@@ -6,7 +6,17 @@ const Testimonial = () => {
       <div className="container">
         <h1 className="text-center font-bold mb-8">What People Are Saying About Us</h1>
    </div>
-   
+   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+<div className="bg-gray-100 p-4">
+<p className="text-lg mb-4">"Standard Intern helped me find an amazing internship opportunity in the field I'm interested in. Their website is easy to navigate and I highly recommend it!"</p>
+</div>
+<div className="bg-gray-100 p-4">
+<p className="text-lg mb-4">"I was struggling to find an internship in my area, but Standard Intern connected me with several local companies. I landed an internship that has been a great learning experience!"</p>
+</div>
+<div className="bg-gray-100 p-4">
+<p className="text-lg mb-4">"I appreciate the range of internship opportunities that Standard Intern provides. They have options in various industries and locations, making it easy to find the right fit."</p>
+</div>
+</div>
    </div>
   )
 }
