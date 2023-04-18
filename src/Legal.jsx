e59b6b2561a80785a37e9f8781ad352d8aff6977
mrbillionn <kyaligonzawarren44@gmail.con>
2023-04-18 18:57:22 +0300

    Update Legal.jsx
    privacy policy and terms of servivce

--- src/Legal.jsx
+++ src/Legal.jsx
@@ -6,6 +6,35 @@ const Legal = () => {
         <div className="py-8 px-4 sm:px-6 lg:px-8">
       <h1 className="text-3xl font-bold mb-4">Terms of Service</h1>
       <p className="mb-4">
+      Welcome to Standard Intern. The following terms and conditions 
+      govern your use of our website, and any services offered through the Site. By using the Site, you agree to be bound by these Terms. If you do not agree to these Terms, you may not use the Site.
+      </p>
+      <p className="mb-4">
+      You may use the Site only for lawful purposes and in accordance with these Terms. You agree not to use the Site:
+      </p>
+      <ul className="list-disc list-inside mb-4">
+        <li>In any way that violates any applicable federal, state, local, or international law or regulation.</li>
+        <li>To impersonate or attempt to impersonate Standard Intern or any Standard Intern employee, agent, or other representative.</li>
+        <li>To engage in any other conduct that restricts or inhibits anyone's use or enjoyment of the Site, or which, as determined by us, may harm Standard Intern or users of the Site or expose them to liability.</li>
+        </ul>
+        <p className="mb-4 font-bold">
+        Intellectual Property Rights
+        </p>
+        <p className="mb-4">
+        The Site and its entire contents, features, and functionality (including but not limited to all information, software, text, displays, images, video, and audio, and the design, selection, and arrangement thereof) are owned by Standard Intern, its licensors, or other providers of such material and are protected by Ugandan and international copyright, trademark, patent, trade secret, and other intellectual property or proprietary rights laws.
+        </p>
+        <p className="mb-4">
+        You may not use any Standard Intern logo or other proprietary graphic or trademark as part of the link without express written permission.
+        </p>
+        <p className="mb-4 font-bold">
+        Limitation of Liability
+        </p>
+        <p className="mb-4">
+        In no event shall Standard Intern, nor its directors, employees, agents, partners, suppliers, or affiliates, be liable for any indirect, incidental, special, consequential, or punitive damages, including without limitation, loss of profits, data, use, goodwill, or other intangible losses, resulting from (i) your access to or use of or inability to access or use the Site; (ii) any conduct or content of any third party on the Site; (iii) any content obtained from the Site; and (iv) unauthorized access, use, or alteration of your transmissions or content, whether based on warranty, contract, tort (including negligence), or any other legal theory, whether or not we have been informed of the possibility of such damage, and even if a remedy set forth herein is found to have failed of its essential purpose.</p>
+        <p className="mb-4 font-bold">
+        Termination
+        </p>
+      <p className="mb-4">
         We reserve the right to terminate or suspend your access to the Site
         immediately, without prior notice or liability, for any reason
         whatsoever, including without limitation if you breach the Terms. All
@@ -14,6 +43,9 @@ const Legal = () => {
         provisions, warranty disclaimers, indemnity, and limitations of
         liability.
       </p>
+      <p className="mb-4 font-bold">
+      Updates to this Terms of Service
+        </p>
       <p className="mb-4">
         We may update this Terms of Service from time to time, and any changes
         will be posted on this page. Your continued use of the Site after any
@@ -25,10 +57,10 @@ const Legal = () => {
         If you have any questions or concerns about our Terms of Service, please
         contact us at{" "}
         <a
-          href="mailto:info@standardintern.com"
+          href="mailto:standardintern@gmail.com"
           className="text-blue-500 hover:text-blue-700"
         >
-          info@standardintern.com
+          standardintern@gmail.com
         </a>
         .
       </p>
@@ -42,28 +74,43 @@ const Legal = () => {
     important, and we are dedicated to ensuring that any personal information
     you provide to us is handled with the utmost care and respect.
   </p>
+  <p className="mb-4 font-bold">
+  Collection of Personal Information
+        </p>
   <p className="mb-4">
     We collect personal information from you when you visit our website or when
     you provide it to us through other means, such as email or phone. The
     information we collect may include your name, email address, phone number,
     and other information relevant to the internship application process.
   </p>
+  <p className="mb-4 font-bold">
+  Use of Personal Information
+        </p>
   <p className="mb-4">
     We use your personal information to provide you with the services and
     information you have requested, to communicate with you about your internship
     application, and to improve our website and services. We do not sell or share
     your personal information with third parties for marketing purposes.
   </p>
+  <p className="mb-4 font-bold">
+  Security of Personal Information
+        </p>
   <p className="mb-4">
     We take the security of your personal information seriously and employ
     reasonable measures to protect it from unauthorized access, disclosure,
     alteration, or destruction. We use encryption, firewalls, and other security
     technologies to safeguard your personal information.
   </p>
+  <p className="mb-4 font-bold">
+  Retention of Personal Information
+        </p>
   <p className="mb-4">
     We retain your personal information for as long as necessary to provide you
     with the services you have requested or as required by law.
   </p>
+  <p className="mb-4 font-bold">
+  Updates to this Privacy Policy
+        </p>
   <p className="mb-4">
     We may update this privacy policy from time to time, and any changes will be
     posted on this page. Your continued use of our website after any changes to
@@ -75,10 +122,10 @@ const Legal = () => {
     If you have any questions or concerns about our privacy policy, please
     contact us at{" "}
     <a
-      href="mailto:info@standardintern.com"
+      href="standardintern@gmail.com"
       className="text-blue-500 hover:text-blue-700"
     >
-      info@standardintern.com
+      standardintern@gmail.com
     </a>
     .
   </p>
