5d0a08ef101f0458dd8787d70c34befc6eb6a968
mrbillionn <kyaligonzawarren44@gmail.con>
2023-04-18 19:18:14 +0300

    Update Testimonial.jsx

--- src/pages/Testimonial.jsx
+++ src/pages/Testimonial.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Testimonial = () => {
   return (
-    <div>Testimonial</div>
+    <div className="bg-white text-black p-3">
+      <div className="container">
+        <h1 className="text-center font-bold mb-8">What People Are Saying About Us</h1>
+   </div>
+   
+   </div>
   )
 }
 
