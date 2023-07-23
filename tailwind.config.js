13f7c4dcae26852f52bf5c0008c69b39530add43
mrbillionn <123182861+mrbillionn@users.noreply.github.com>
2023-07-23 14:49:55 +0300

    Update tailwind.config.js
    Color for the orange grad

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-diagonal-orange': 'linear-gradient(45deg, #FFA500, #FF4500)',
+      },
+      boxShadow: {
+        'glass': '0 2px 6px rgba(0, 0, 0, 0.2)',
+      },
+    },
   },
   plugins: [],
-}
+};
 
